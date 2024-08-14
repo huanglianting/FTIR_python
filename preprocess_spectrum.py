677f4cad0f9b75d81fa2241a4c950d28cea4620b
dell <huanglianting@sjtu.edu.cn>
2024-08-14 22:13:44 +0800

    更正回退后的bug，修正为在900-1800指纹区处理，同时删去应用拉平2200-2400波数段的函数这段暂时无用的代码

--- preprocess_spectrum.py
+++ preprocess_spectrum.py
@@ -8,12 +8,10 @@ from vector_normalized import vector_normalized
 def preprocess_spectrum(x, AB, threshold1, threshold2, order, frame_len, save_path):
     # 只处理threshold1-threshold2之间的光谱
     x, filtered_spectrum = filter_threshold(AB, x, threshold1, threshold2)
-    # 把2200-2400 cm^-1二氧化碳吸收峰的光谱拉平
-    flattened_spectrum = flatten_region(filtered_spectrum, x, 2200, 2400, save_path)
     # 矢量归一化
-    normalized_spectrum = vector_normalized(flattened_spectrum, x, save_path)
+    normalized_spectrum = vector_normalized(filtered_spectrum, x, save_path)
     # Savitzky-Golay平滑
     smoothed_spectrum = SG_smooth(normalized_spectrum, x, order, frame_len, save_path)
-    # 计算二阶导数
+    # 计算二阶导数。求了导感觉效果没那么好；另外，如果求了二阶导数，那么k-means那里，纵坐标还是AB就不太对了
     # x, derivative_spectrum = derivative(smoothed_spectrum, x, save_path)
     return x, smoothed_spectrum
