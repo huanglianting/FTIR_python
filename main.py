4275b315fa1e20a35b5551fb84491f22aed241ec
923048992@qq.com <923048992@qq.com>
2025-07-18 23:57:01 +0800

    MultiModal - 准确率: 0.9271, 精确率: 0.9695, 召回率(Sensitivity): 0.8819, 特异性: 0.9722, F1: 0.9236, AUC: 0.9920 MultiModal - 类别0准确率: 0.9722, 类别1准确率: 0.8819

--- main.py
+++ main.py
@@ -502,14 +502,14 @@ def run_grid_search_for_model(model_name, model_class, ftir_train, mz_train, y_t
 # 对所有模型，利用 k-fold 交叉验证调参，确定最优参数
 models_to_evaluate = {
     "MultiModal": MultiModalModel,
-    "FTIROnly": SingleFTIRModel,
-    "MZOnly": SingleMZModel,
-    "ConcatFusion": ConcatFusion,
-    "GateOnlyFusion": GateOnlyFusion,
-    "CoAttnOnlyFusion": CoAttnOnlyFusion,
-    "SelfAttnOnlyFusion": SelfAttnOnlyFusion,
-    "SelfAttnFusion": SelfAttnFusion,
-    "SVM": SVMClassifier
+    # "FTIROnly": SingleFTIRModel,
+    # "MZOnly": SingleMZModel,
+    # "ConcatFusion": ConcatFusion,
+    # "GateOnlyFusion": GateOnlyFusion,
+    # "CoAttnOnlyFusion": CoAttnOnlyFusion,
+    # "SelfAttnOnlyFusion": SelfAttnOnlyFusion,
+    # "SelfAttnFusion": SelfAttnFusion,
+    # "SVM": SVMClassifier
 }
 
 all_model_dfs = []
