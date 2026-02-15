3ec223a8980946bc520a218e117e4ca8f2c5040f
923048992@qq.com <923048992@qq.com>
2026-02-15 23:06:09 +0800

    NAN报错

--- main.py
+++ main.py
@@ -1516,15 +1516,15 @@ def run_grid_search_for_model(model_name, model_class, ftir_train, mz_train, y_t
 
 # 对所有模型，利用 k-fold 交叉验证调参，确定最优参数
 models_to_evaluate = {
-    "MultiModal": MultiModalModel,
-    "MultiModalLite": MultiModalLite,
+    # "MultiModal": MultiModalModel,
+    # "MultiModalLite": MultiModalLite,
     # 经典机器学习基线
     # "SVM": SVMClassifier,
     # "LogReg": LogRegClassifier,
     # "RandomForest": RFClassifier,
     # "KNN": KNNClassifier,
-    # "GaussianNB": NBClassifier,   # 这个老是报错NAN 修复了很多次都不行，最新一次修复在version 67。
-    "GBDT": GBDTClassifier,
+    "GaussianNB": NBClassifier,   # 这个老是报错NAN 修复了很多次都不行，最新一次修复在version 67。
+    # "GBDT": GBDTClassifier,   # 这个还没跑过，不知道报不报错
     # 如需启用其他深度模型，取消注释以下条目
     # "BiModalCMACF": BiModalCMACF,
     # "CMSTF": CMSTF,
