7ad434edb3879bed89fb3e09b942654bd1eb0660
923048992@qq.com <923048992@qq.com>
2026-02-17 16:19:56 +0800

    distance optimal

--- main.py
+++ main.py
@@ -1160,7 +1160,7 @@ param_grid = {
 
 RUN_FIXED_TEST_EVAL = True
 RUN_REPEATED_OUTER_CV = True
-THRESHOLD_METHOD = "constrained_f1"  # "youden"、"balanced"、"distance_optimal"
+THRESHOLD_METHOD = "distance_optimal"  # "youden"、"balanced"、"constrained_f1"
 all_params = [dict(zip(param_grid.keys(), values))
               for values in itertools.product(*param_grid.values())]
 best_params = None
