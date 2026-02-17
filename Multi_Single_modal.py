83bbbcd75245a6cc2f83df2efb1564fb42fe7f18
923048992@qq.com <923048992@qq.com>
2026-02-17 13:38:27 +0800

    两个多模态改成heads=4，youden作为阈值

--- Multi_Single_modal.py
+++ Multi_Single_modal.py
@@ -120,7 +120,7 @@ class MultiModalModel(nn.Module):
         super(MultiModalModel, self).__init__()
         self.ftir_extractor = FTIREncoder(ftir_input_dim)
         self.mz_extractor = MZEncoder(mz_input_dim)
-        self.fuser = HybridFusion(dim=64, num_heads=2)
+        self.fuser = HybridFusion(dim=64, num_heads=4)
         self.classifier = nn.Sequential(
             nn.Linear(128, 64),
             nn.BatchNorm1d(64),
@@ -144,7 +144,7 @@ class MultiModalLite(nn.Module):
         super(MultiModalLite, self).__init__()
         self.ftir_extractor = FTIREncoder(ftir_input_dim)
         self.mz_extractor = MZEncoder(mz_input_dim)
-        self.fuser = HybridFusion(dim=64, num_heads=2)
+        self.fuser = HybridFusion(dim=64, num_heads=4)
         self.classifier = nn.Sequential(
             nn.Linear(128, 64),
             nn.BatchNorm1d(64),
@@ -160,8 +160,6 @@ class MultiModalLite(nn.Module):
         return output
 
 # ==================单模态模型定义====================================
-
-
 class SingleFTIRModel(nn.Module):
     def __init__(self, input_dim):
         super(SingleFTIRModel, self).__init__()
